5f244eaa2873caea78e39711aa2d05337c7705ad
25mordad <25mordad@gmail.com>
2022-02-06 08:12:17 +0330

    Write tests for edit user details

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -15,5 +15,35 @@ class UserTest extends TestCase
 
         $response->assertJson(fn (AssertableJson $json) => $json->has(2));
     }
+    
+    public function test_edit_details_for_valid_user()
+    {
+        $valid_user_id = 7;
+        $details = [
+            'first_name' => 'Diego',
+            'last_name' => 'Lopez',
+            'phone_number' => '003434343434',
+            'country_iso3' => 'ESP'
+        ];
+        $response = $this->post("/users/$valid_user_id/edit-details", $details);
+        $response->assertStatus(200);
+        //TODO assert
+    }
+
+    public function test_edit_details_for_invalid_user()
+    {
+        $invalid_user_id = 2;
+        $details = [
+            'first_name' => 'Diego',
+            'last_name' => 'Lopez',
+            'phone_number' => '003434343434',
+            'country_iso3' => 'ESP'
+        ];
+        $response = $this->post("/users/$invalid_user_id/edit-details", $details);
+        $response->assertStatus(200);
+        //TODO assert
+
+    }
+
 
 }
